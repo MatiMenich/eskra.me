58d1c84cc8e6b99bdbddf70a96d1ceb15d2f61c0
Bernardo Jaime Küpfer Besa <bdokupferb@gmail.com>
2014-05-31 11:32:12 -0400

    initial template for application --> check   NOTE: it should start without any stickie! --> TODO: remove initail stickies
    added few elements to the model. (poblated initial db)

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -62,13 +62,13 @@ class BoardsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_board
-      @board = Board.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_board
+    @board = Board.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def board_params
-      params.require(:board).permit(:name, :uid)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def board_params
+    params.require(:board).permit(:name, :uid)
+  end
 end
