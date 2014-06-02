cccda256bac1aef5395267fe4869c3d1823180d3
Matias Menich Mendez <matimenich@gmail.com>
2014-06-01 23:35:22 -0400

    [ADD] General persistance for columns and loading variables from controller (stickies,stories,columns)

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,46 +1,38 @@
-$(function() {
+var ready = function() {
 
-     /* for adding the initial board to the data base */
-    function addBoard(){
-        $.ajax({
-            url:    "/boards",
-            type:   "POST",
-            data:   {board: {name: "pusheen the mouse (?)", uid: 1}},
-        });
+  var storyNumber = 1;
 
-    }
+/*
+  function recoverStickiesData(){
 
+    var dani;
+    $.ajax({
+      dataType: "json",
+      type:   'GET',
+      url:    '/stickies/1',
+      success:function(data){
+              // alert( JSON.stringify(data) );
+              //alert( data.name );
 
-    function recoverStickiesData(){
+              dani = data;
 
-        var dani;
-        $.ajax({
-            dataType: "json",
-            type:   'GET',
-            url:    '/stickies/1',
-            success:function(data){
-                // alert( JSON.stringify(data) );
-                //alert( data.name );
+              var title = data.name;
+              var body  = data.text;
+              var panel = $('<div class="panel panel-default"></div>');
+              var panelHeader = $('<div class="panel-heading">'+title+'<i class="panel-button"></i></div>');
+              var panelBody = $('<div class="panel-body">'+body+'</div>');
 
-                dani = data;
+              panel.append(panelHeader);
+              panel.append(panelBody);
 
-                var title = data.name;
-                var body  = data.text;
-                var panel = $('<div class="panel panel-default"></div>');
-                var panelHeader = $('<div class="panel-heading">'+title+'<i class="panel-button"></i></div>');
-                var panelBody = $('<div class="panel-body">'+body+'</div>');
-
-                panel.append(panelHeader);
-                panel.append(panelBody);
-
-                $('.add-panel-' + 1).closest('tr').find("td:eq(1)").append(panel);
+              $('.add-panel-' + 1).closest('tr').find("td:eq(1)").append(panel);
 
 
             }
-        });
+          });
 
-    };
-
+  };
+*/
 
   function addLayoutBehaviour(){
     $( ".column" ).sortable({
@@ -64,17 +56,46 @@ $(function() {
       var panel = $( this ).closest('.panel');
       panel.toggleClass( "panel-default panel-success" );
     });
+
+    $("[data-xeditable=true]").each(function() {
+    return $(this).editable({
+      mode: 'inline',
+      ajaxOptions: {
+        type: "PUT",
+        dataType: "json"
+      },
+      params: function(params) {
+        var railsParams;
+        railsParams = {};
+        railsParams[$(this).data("model")] = {};
+        railsParams[$(this).data("model")][params.name] = params.value;
+        return railsParams;
+      }
+    });
+  });
   };
 
   function addButtonBehaviour(){
 
     $(".delete-column").click(function(){
-      var columnIndex = $(this).closest("th").prevAll("th").length;
-      $(this).parents("table").find("tr").each(function () {
-        $(this).find("td:eq("+columnIndex+"), th:eq("+columnIndex+")").fadeOut('slow', function() {
-          $(this).remove();
-        });
+
+      var button = $(this);
+      var columnId = button.parent().attr('board-id');
+
+      $.ajax({
+        dataType: "json",
+        type:   'delete',
+        url:    '/columns/'+columnId,
+        success:function(data){
+          var columnIndex = button.closest("th").prevAll("th").length;
+          button.parents("table").find("tr").each(function () {
+            $(this).find("td:eq("+columnIndex+"), th:eq("+columnIndex+")").fadeOut('slow', function() {
+              $(this).remove();
+            });
+          });
+        }
       });
+      
     });
 
     $(".delete-story").click(function(){
@@ -84,39 +105,50 @@ $(function() {
         $(this).remove();
       });
     });
-    
+
   };
 
-  addBoard();
+  //recoverStickiesData();
   addLayoutBehaviour();
   addButtonBehaviour();
-  recoverStickiesData();
+
 
 
 
   $('#add_column').click(function () {
 
-    $('.column_field').find('tr').each(function () {
-      var newColumn = $('<td class="column"><div class="column-content"></div></td>');
-      newColumn.hide();
-      $(this).append(newColumn)
-      newColumn.fadeIn('slow');
-    });
-
-    $('.title_field').find('tr').each(function () {
-      var newTitle = $('<th><div class="column-title h4">Title X <button class="btn btn-xs btn-danger delete-column pull-right"><span class="glyphicon glyphicon-remove"></span></button></div></th>');
-      newTitle.hide();
-      $(this).append(newTitle);
-      newTitle.fadeIn('slow');
-    });
-
-    addLayoutBehaviour();
-    addButtonBehaviour();
-
+    var columnTitle = "Column title";
+    var boardId = $(this).attr("board-id");
+    
     $.ajax({
       url:    "/columns",
       type:   "POST",
-      data:   {column: {name:"creatividad excesiva", order: 1}},
+      data:   {column: {name: columnTitle, board_id: boardId, order: 4}},
+      success: function(response){ 
+
+          columnId = response.id;
+
+          $('.column_field').find('tr').each(function () {
+            var newColumn = $('<td class="column"><div class="column-content"></div></td>');
+            newColumn.hide();
+            $(this).append(newColumn)
+            newColumn.fadeIn('slow');
+          });
+
+          $('.title_field').find('tr').each(function () {
+            var removeButton = $('<button class="btn btn-xs btn-danger delete-column pull-right"><span class="glyphicon glyphicon-remove"></span></button>')
+            var newTitle = $('<div class="column-title h4" id="'+columnId+'"></div>');
+            var titleContent = $('<a href="#" data-xeditable="true" data-pk="'+columnId+'" data-model="column" data-name="name" data-url="/columns/'+columnId+'" data-title="Enter name">'+columnTitle+'</a>');
+            newTitle.append(titleContent);
+            newTitle.append(removeButton);
+            newTitle.hide();
+            $(this).append($('<th></th>').append(newTitle));
+            newTitle.fadeIn('slow');
+          });
+
+          addLayoutBehaviour();
+          addButtonBehaviour();
+        }
     });
 
   });
@@ -125,7 +157,7 @@ $(function() {
     storyNumber++;
 
     var newRow = $('<tr></tr>');
-    var title = $('<td>Story X</td>');
+    var title = $('<td>Story X </td>');
     var buttonGroup = $('<div class="btn-group"><button class="btn btn-xs btn-warning delete-story"><span class="glyphicon glyphicon-trash"> </span></button><button class="btn btn-xs btn-primary add-panel-'+storyNumber+'"><span class="glyphicon glyphicon-plus"> </span></button></div>');
     var number_of_columns = $('.title_field').find('tr').first().find('th').length
     title.append(buttonGroup);
@@ -155,15 +187,15 @@ $(function() {
       $(this).closest('tr').find("td:eq(1)").append(panel);
 
       $.ajax({
-          url:    "/stickies",
-          type:   "POST",
-          data:   {stickie: {name: 'dani', text: 'gatito pipipim', column_id: 1/*default value*/,  row_id: storyNumber}},
-          success:function(resp){ alert('!!') }
+        url:    "/stickies",
+        type:   "POST",
+        data:   {sticky: {name: 'dani', text: 'gatito pipipim', column_id: 1/*default value*/,  row_id: storyNumber}},
+        success: function(resp){ 
+          alert("!!");
+        }
       });
 
-    addLayoutBehaviour();
-
-
+      addLayoutBehaviour();
     });
   });
 
@@ -183,4 +215,8 @@ $(function() {
     addLayoutBehaviour();
   });
 
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
