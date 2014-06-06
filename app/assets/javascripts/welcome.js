a143f2ea7e93bc5883d95afa493e535ff69e59fa
Matias Menich Mendez <matimenich@gmail.com>
2014-06-06 12:47:34 -0400

    [FIX] Delete for dynamic stickies

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -13,9 +13,9 @@ var ready = function() {
       revert: true,
       update: function (event, ui) {
 
-          var columnId = ui.item.parents(".column").attr("column-id");
-          var storyId = ui.item.parents("tr").attr("story-id");
-          var stickyId = ui.item.attr("sticky-id");
+        var columnId = ui.item.parents(".column").attr("column-id");
+        var storyId = ui.item.parents("tr").attr("story-id");
+        var stickyId = ui.item.attr("sticky-id");
           //console.log('column-id:'+columnId);
           //console.log('story-id:'+storyId);
           //console.log('sticky-id:'+stickyId);
@@ -27,7 +27,7 @@ var ready = function() {
             }
           });
         }
-    });
+      });
 
     $( ".panel" )
     .find( ".panel-button" )
@@ -58,7 +58,7 @@ var ready = function() {
     .find( ".panel-button" )
     .append(toggleButton).append(dropdownDiv);
 
-	
+    
 
     $( ".panel-toggle" ).click(function() {
       var icon = $( this ).find('span');
@@ -98,6 +98,22 @@ var ready = function() {
         }
       });
       
+    });
+
+    $(".delete-sticky").click(function(){
+      var button = $(this);
+      var panel = button.closest(".panel-default");
+      var stickyId = panel.attr('sticky-id');
+      $.ajax({
+        dataType: "json",
+        type: 'delete',
+        url: '/stickies/'+stickyId,
+        success: function (data) {
+          panel.fadeOut('slow', function(){
+            panel.remove();
+          });
+        }
+      });
     });
   };
 
@@ -176,162 +192,164 @@ var ready = function() {
       data:   {column: {name: columnTitle, board_id: boardId, order: 4}},
       success: function(response){ 
 
-          var columnId = response.id;
+        var columnId = response.id;
 
-          $('.column_field').find('tr').each(function () {
-            var newColumn = $('<td class="column"><div class="column-content"></div></td>');
-            newColumn.hide();
-            $(this).append(newColumn)
-            newColumn.fadeIn('slow');
-          });
+        $('.column_field').find('tr').each(function () {
+          var newColumn = $('<td class="column"><div class="column-content"></div></td>');
+          newColumn.hide();
+          $(this).append(newColumn)
+          newColumn.fadeIn('slow');
+        });
 
-          $('.title_field').find('tr').each(function () {
-            var removeButton = $('<button class="btn btn-xs btn-danger delete-column pull-right"><span class="glyphicon glyphicon-remove"></span></button>')
-            var newTitle = $('<div class="column-title h4" column-id="'+columnId+'"></div>');
-            var titleContent = $('<a href="#" data-xeditable="true" data-pk="'+columnId+'" data-model="column" data-name="name" data-url="/columns/'+columnId+'" data-title="Enter name">'+columnTitle+'</a>');
-            newTitle.append(titleContent);
-            newTitle.append(removeButton);
-            newTitle.hide();
-            $(this).append($('<th></th>').append(newTitle));
-            newTitle.fadeIn('slow');
-          });
+        $('.title_field').find('tr').each(function () {
+          var removeButton = $('<button class="btn btn-xs btn-danger delete-column pull-right"><span class="glyphicon glyphicon-remove"></span></button>')
+          var newTitle = $('<div class="column-title h4" column-id="'+columnId+'"></div>');
+          var titleContent = $('<a href="#" data-xeditable="true" data-pk="'+columnId+'" data-model="column" data-name="name" data-url="/columns/'+columnId+'" data-title="Enter name">'+columnTitle+'</a>');
+          newTitle.append(titleContent);
+          newTitle.append(removeButton);
+          newTitle.hide();
+          $(this).append($('<th></th>').append(newTitle));
+          newTitle.fadeIn('slow');
+        });
 
-          addLayoutBehaviour();
-          addButtonBehaviour();
-        }
-    });
-
-  });
-
-  $('#add_story').click(function () {
-    var boardId = $(this).attr("board-id");
-    $.ajax({
-      url:    "/rows",
-      type:   "POST",
-      data:   {row: {board_id: boardId}},
-      success: function (response) {
-        var newRow = $('<tr story-id="'+response.id+'"></tr>');
-        var editable = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="row" data-name="name" data-url="/rows/'+response.id+'" data-title="Enter name"></a>');
-        var title = $('<td></td>');
-        editable.append(response.name);
-        title.append(editable);
-        var buttonGroup = $('<div class="btn-group"></div>');
-        var deleteButton = $('<button class="btn btn-xs btn-warning delete-story"><span class="glyphicon glyphicon-trash"> </span>');
-        var addButton = $('</button><button class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-plus"> </span></button>');
-        var number_of_columns = $('.title_field').find('tr').first().find('th').length
-        
-        /* Button append */
-        buttonGroup.append(deleteButton);
-        buttonGroup.append(addButton);
-
-        title.append(buttonGroup);
-
-        var columnIds = [];
-        $('.column_field').find('tr').first().find('.column').each(function () { columnIds.push( $(this).attr('column-id')) })
-
-        newRow.append(title);
-        for(var i = 0 ; i < number_of_columns-1 ; i++){
-          var newColumn = $('<td class="column" column-id="'+columnIds[i]+'"></td>');
-          newRow.append(newColumn)
-        }
-
-        newRow.hide();
-        $('.column_field').append(newRow);
-        newRow.fadeIn('slow');
         addLayoutBehaviour();
         addButtonBehaviour();
+      }
+    });
 
-        addButton.click(function(){
-          
-          var currentStory = $(this).closest('tr');
-          var storyId = currentStory.attr('story-id');
-          var starterColumnId = $(this).closest('tr').find("td:eq(1)").attr("column-id");
+});
 
-          $.ajax({
-            url:    "/stickies",
-            type:   "POST",
-            data:   {sticky: {column_id: starterColumnId, row_id: storyId}},
-            success: function(response){ 
-              var panel = $('<div class="panel panel-default" sticky-id="'+response.id+'"></div>');
-              var editableHeader = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="name" data-url="/stickies/'+response.id+'" data-title="Enter name">'+response.name+'</a>')
-              var panelHeader = $('<div class="panel-heading"><i class="panel-button"></i></div>');
-              panelHeader.prepend(editableHeader);
-              var editableBody = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="text" data-url="/stickies/'+response.id+'" data-title="Enter text" data-type="textarea" data-showbuttons="bottom" data-anim="500">'+response.text+'</a>');
-              var panelBody = $('<div class="panel-body"></div>');
-              panelBody.append(editableBody);
+$('#add_story').click(function () {
+  var boardId = $(this).attr("board-id");
+  $.ajax({
+    url:    "/rows",
+    type:   "POST",
+    data:   {row: {board_id: boardId}},
+    success: function (response) {
+      var newRow = $('<tr story-id="'+response.id+'"></tr>');
+      var editable = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="row" data-name="name" data-url="/rows/'+response.id+'" data-title="Enter name"></a>');
+      var title = $('<td></td>');
+      editable.append(response.name);
+      title.append(editable);
+      var buttonGroup = $('<div class="btn-group"></div>');
+      var deleteButton = $('<button class="btn btn-xs btn-warning delete-story"><span class="glyphicon glyphicon-trash"> </span>');
+      var addButton = $('</button><button class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-plus"> </span></button>');
+      var number_of_columns = $('.title_field').find('tr').first().find('th').length
+      
+      /* Button append */
+      buttonGroup.append(deleteButton);
+      buttonGroup.append(addButton);
 
-              panel.append(panelHeader);
-              panel.append(panelBody);
+      title.append(buttonGroup);
 
-              currentStory.find("td:eq(1)").append(panel);
+      var columnIds = [];
+      $('.column_field').find('tr').first().find('.column').each(function () { columnIds.push( $(this).attr('column-id')) })
 
-              addLayoutBehaviour();
-            }
-          });
+      newRow.append(title);
+      for(var i = 0 ; i < number_of_columns-1 ; i++){
+        var newColumn = $('<td class="column" column-id="'+columnIds[i]+'"></td>');
+        newRow.append(newColumn)
+      }
 
-          addLayoutBehaviour();
+      newRow.hide();
+      $('.column_field').append(newRow);
+      newRow.fadeIn('slow');
+      addLayoutBehaviour();
+      addButtonBehaviour();
+
+      addButton.click(function(){
+        
+        var currentStory = $(this).closest('tr');
+        var storyId = currentStory.attr('story-id');
+        var starterColumnId = $(this).closest('tr').find("td:eq(1)").attr("column-id");
+
+        $.ajax({
+          url:    "/stickies",
+          type:   "POST",
+          data:   {sticky: {column_id: starterColumnId, row_id: storyId}},
+          success: function(response){ 
+            var panel = $('<div class="panel panel-default" sticky-id="'+response.id+'"></div>');
+            var editableHeader = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="name" data-url="/stickies/'+response.id+'" data-title="Enter name">'+response.name+'</a>')
+            var panelHeader = $('<div class="panel-heading"><i class="panel-button"></i></div>');
+            panelHeader.prepend(editableHeader);
+            var editableBody = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="text" data-url="/stickies/'+response.id+'" data-title="Enter text" data-type="textarea" data-showbuttons="bottom" data-anim="500">'+response.text+'</a>');
+            var panelBody = $('<div class="panel-body"></div>');
+            panelBody.append(editableBody);
+
+            panel.append(panelHeader);
+            panel.append(panelBody);
+
+            currentStory.find("td:eq(1)").append(panel);
+
+            addLayoutBehaviour();
+          }
         });
+
+addLayoutBehaviour();
+});
+}
+
+});
+
+
+});
+
+var viewState = true;
+
+$('#toggle_collapse').click(function() {
+  $('[class*="glyphicon-chevron-"]').each(function () {
+    if(viewState){
+      $(this).removeClass( "glyphicon-chevron-down" );
+      $(this).addClass( "glyphicon-chevron-up" );
+      $(this).closest( ".panel" ).find( ".panel-body" ).hide();
+    }
+    else {
+      $(this).removeClass( "glyphicon-chevron-up" );
+      $(this).addClass( "glyphicon-chevron-down" );
+      $(this).closest( ".panel" ).find( ".panel-body" ).show();
+    }
+  });
+  viewState = !viewState;
+});
+
+$('#toggle_edit').click(function () {
+  $("[data-xeditable=true]").editable('toggleDisabled');
+});
+
+/* initial panel's buttons */
+$("[add-sticky=true]").each(function () { 
+  return $(this).click(function(){
+
+    var currentStory = $(this).closest('tr');
+
+    var starterColumnId = currentStory.find("td:eq(1)").attr("column-id");
+    var storyId = currentStory.attr('story-id');
+
+    $.ajax({
+      url:    "/stickies",
+      type:   "POST",
+      data:   {sticky: {column_id: starterColumnId, row_id: storyId}},
+      success: function(response){ 
+        var panel = $('<div class="panel panel-default" sticky-id="'+response.id+'"></div>');
+        var editableHeader = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="name" data-url="/stickies/'+response.id+'" data-title="Enter name">'+response.name+'</a>')
+        var panelHeader = $('<div class="panel-heading"><i class="panel-button"></i></div>');
+        panelHeader.prepend(editableHeader);
+        var editableBody = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="text" data-url="/stickies/'+response.id+'" data-title="Enter text" data-type="textarea" data-showbuttons="bottom" data-anim="500" >'+response.text+'</a>');
+        var panelBody = $('<div class="panel-body"></div>');
+        panelBody.append(editableBody);
+
+        panel.append(panelHeader);
+        panel.append(panelBody);
+
+        currentStory.find("td:eq(1)").append(panel);
+
+        addLayoutBehaviour();
+
+        
       }
-
     });
-
-    
-  });
-
-  var viewState = true;
-
-  $('#toggle_collapse').click(function() {
-    $('[class*="glyphicon-chevron-"]').each(function () {
-      if(viewState){
-        $(this).removeClass( "glyphicon-chevron-down" );
-        $(this).addClass( "glyphicon-chevron-up" );
-        $(this).closest( ".panel" ).find( ".panel-body" ).hide();
-      }
-      else {
-        $(this).removeClass( "glyphicon-chevron-up" );
-        $(this).addClass( "glyphicon-chevron-down" );
-        $(this).closest( ".panel" ).find( ".panel-body" ).show();
-      }
-    });
-    viewState = !viewState;
-  });
-
-  $('#toggle_edit').click(function () {
-    $("[data-xeditable=true]").editable('toggleDisabled');
-  });
-
-  /* initial panel's buttons */
-  $("[add-sticky=true]").each(function () { 
-    return $(this).click(function(){
-
-      var currentStory = $(this).closest('tr');
-
-      var starterColumnId = currentStory.find("td:eq(1)").attr("column-id");
-      var storyId = currentStory.attr('story-id');
-
-      $.ajax({
-        url:    "/stickies",
-        type:   "POST",
-        data:   {sticky: {column_id: starterColumnId, row_id: storyId}},
-        success: function(response){ 
-          var panel = $('<div class="panel panel-default" sticky-id="'+response.id+'"></div>');
-          var editableHeader = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="name" data-url="/stickies/'+response.id+'" data-title="Enter name">'+response.name+'</a>')
-          var panelHeader = $('<div class="panel-heading"><i class="panel-button"></i></div>');
-          panelHeader.prepend(editableHeader);
-          var editableBody = $('<a href="#" data-xeditable="true" data-pk="'+response.id+'" data-model="sticky" data-name="text" data-url="/stickies/'+response.id+'" data-title="Enter text" data-type="textarea" data-showbuttons="bottom" data-anim="500" >'+response.text+'</a>');
-          var panelBody = $('<div class="panel-body"></div>');
-          panelBody.append(editableBody);
-
-          panel.append(panelHeader);
-          panel.append(panelBody);
-
-          currentStory.find("td:eq(1)").append(panel);
-
-          addLayoutBehaviour();
-        }
-      });
-    });
-  });
+});
+});
 
 };
 
